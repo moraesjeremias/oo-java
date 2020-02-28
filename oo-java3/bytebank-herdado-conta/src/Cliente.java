5d65489e95a3723fc8fd6a7edf0396e312140fc5
Jeremias Moraes <jeremias@volanty.com>
2020-02-28 16:25:24 -0300

    Exercício Herança e Polimorfismo

---
+++ oo-java3/bytebank-herdado-conta/src/Cliente.java
@@ -0,0 +1,42 @@
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String profissao;
+    private static int total = 0;
+
+    public Cliente(String nome, String cpf, String profissao) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.profissao = profissao;
+        total += 1;
+        System.out.println("Um novo cliente foi criado");
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissao() {
+        return this.profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public static int getTotal() {
+        return Cliente.total;
+    }
+}
---
+++ oo-java3/bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,67 @@
+public class Conta {
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+
+    public Conta(int agencia, int numero) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = 100;
+        //System.out.println("Estou criando uma conta");
+    }
+
+    public double deposita(double valor) {
+        return this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saca(valor)){
+            destino.deposita(valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double valor) {
+        this.saldo = deposita(valor);
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+}
